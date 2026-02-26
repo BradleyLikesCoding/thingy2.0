55d69054e8195ddc28b68b3431879737c0a0be89
Bradley <bradleysemail2@gmail.com>
2026-02-26 08:30:03 -0500

    fixed stuff

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts(new URL('./cdntest.js', self.location.href).href);
+importScripts("./cdntest.js");
 
 self.addEventListener("install", event => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
